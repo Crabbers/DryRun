8c7a1eef97ae01720ec6aecd2a41030bdd8f41c3
jstennant <jono.tennant@gmail.com>
2015-04-16 23:03:00 +0800

    Initial checkin of example project

---
+++ SomeLibrary.Tests/SomeClassTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SomeLibrary.Tests
+{
+    [TestClass]
+    public class SomeClassTest
+    {
+        /// <summary>
+        /// Unit test for the Normal case of the Random method
+        /// </summary>
+        [TestMethod]
+        public void Unit_Random_Normal()
+        {
+            SomeClass target = new SomeClass();
+
+            int actual = target.Random();
+            int expected = 4;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ SomeLibrary/SomeClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SomeLibrary
+{
+    public class SomeClass
+    {
+        public int Random()
+        {
+            // Selected by fair dice-roll, guarunteed to be random
+            return 4;
+        }
+    }
+}
